f98bafd4ece4e6e0d230b06d6c78ca50c988f895
Vladimir Titenko <tvolodi@gmail.com>
2024-09-23 14:08:48 +0500

    Added BaseAppController DefaultDTO

---
+++ ServerExt/BaseApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server_ext
+{
+    public static class BaseApp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
